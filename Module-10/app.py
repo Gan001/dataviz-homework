49849d0c04493b77320c1f9c4860e0b607af5aae
Ganesh Crooks <gcroo001@gmail.com>
2019-03-30 03:19:24 -0400

    added new routes in app

--- Module-10/app.py
+++ Module-10/app.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine, inspect, func
 import numpy as np
 import pandas as pd
 import datetime as dt
+from collections import defaultdict
 
 #create engine
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
@@ -33,11 +34,27 @@ results = session.query(Measurement.date, Measurement.prcp).\
           filter(Measurement.date >= one_year_ago).filter(Measurement.prcp != "None").all()
 
 # Save the query results as a Pandas DataFrame and set the index to the date column
-df = pd.DataFrame(results, columns = ['date', 'precipitation'])
-df.set_index('date', inplace=True, )
+# df = pd.DataFrame(results, columns = ['date', 'precipitation'])
+# df.set_index('date', inplace=True, )
+prcp_df = pd.DataFrame(results).sort_values('date')
+prcp_dict = prcp_df.to_dict('records')
+
+prcp_default =defaultdict(list)
+for i in range(len(prcp_dict)):
+    prcp_default[prcp_dict[i]['date']].append(prcp_dict[i]['prcp'])
+
+adjust_prcp_dict = dict(prcp_default)
 
 # Sort the dataframe by date
-sorted_df = df.sort_values('date')
+#sorted_df = df.sort_values('date')
+#List of stations
+stations = session.query(Measurement.station).group_by(Measurement.station).all()
+station_list =[]
+for station in stations:
+    station_list.append(station[0])   
+                           
+#list of temperature observations
+tobs = session.query(Measurement.station,Measurement.tobs).order_by(Measurement.station).filter(Measurement.date >= one_year_ago).all()
 
 #Setup Flask
 app = Flask(__name__)
@@ -50,9 +67,15 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    return "hello"
+    return jsonify(adjust_prcp_dict)
 
+@app.route("/api/v1.0/stations")
+def station_lists():
+    return jsonify(station_list)
 
+@app.route("/api/v1.0/tobs")
+def observations():
+    return jsonify(tobs)
 
 if __name__ == "__main__":
     app.run(debug=True)
