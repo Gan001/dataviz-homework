04357eed2af6e03242d9dc64ee6213dbf9272ddd
Ganesh Crooks <gcroo001@gmail.com>
2019-03-30 17:58:34 -0400

    Advanced-Data-Storage HW

--- Module-10/app.py
+++ Module-10/app.py
@@ -22,36 +22,12 @@ Station = Base.classes.station
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
-# Design a query to retrieve the last 12 months of precipitation data and plot the results
+# Design a query to retrieve the last 12 months of precipitation data 
 date_query = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
 last_year = date_query[0]
 last_year_split = last_year.split("-")
 last_year_split[0] = str(int(last_year_split[0])-1)
 one_year_ago = "-".join(last_year_split)
-
-# Perform a query to retrieve the data and precipitation scores
-results = session.query(Measurement.date, Measurement.prcp).\
-        filter(Measurement.date >= one_year_ago).filter(Measurement.prcp != "None").all()
-
-# Save the query results as a Pandas DataFrame and create a dictionary
-prcp_df = pd.DataFrame(results).sort_values('date')
-prcp_dict = prcp_df.to_dict('records')
-
-prcp_default =defaultdict(list)
-for i in range(len(prcp_dict)):
-    prcp_default[prcp_dict[i]['date']].append(prcp_dict[i]['prcp'])
-
-adjust_prcp_dict = dict(prcp_default)
-# Sort the dataframe by date
-#sorted_df = df.sort_values('date')
-#List of stations
-stations = session.query(Measurement.station).group_by(Measurement.station).all()
-station_list =[]
-for station in stations:
-    station_list.append(station[0])   
-                           
-#list of temperature observations
-tobs = session.query(Measurement.tobs).order_by(Measurement.station).filter(Measurement.date >= one_year_ago).all()
 
 #Setup Flask
 app = Flask(__name__)
@@ -72,28 +48,53 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    # Perform a query to retrieve the data and precipitation scores
+    results = session.query(Measurement.date, Measurement.prcp).\
+            filter(Measurement.date >= one_year_ago).filter(Measurement.prcp != "None").all()
+
+    # Save the query results as a Pandas DataFrame and create a dictionary
+    prcp_df = pd.DataFrame(results).sort_values('date')
+    prcp_dict = prcp_df.to_dict('records')
     
+    # default dict appends list if key is same otherwise makes new key, use this or lose prcp values due to multiple non-unique keys
+    prcp_default = defaultdict(list)
+    for i in range(len(prcp_dict)):
+        prcp_default[prcp_dict[i]['date']].append(prcp_dict[i]['prcp'])
+    
+    #convert to regular dict
+    adjust_prcp_dict = dict(prcp_default)
+
     #return jsonified dictionary
     return jsonify(adjust_prcp_dict)
 
 @app.route("/api/v1.0/stations")
 def station_lists():
+    stations = session.query(Measurement.station).group_by(Measurement.station).all()
+    station_list =[]
+    for station in stations:
+        station_list.append(station[0])
+        
     return jsonify(station_list)
 
 @app.route("/api/v1.0/tobs")
 def observations():
+    tobs = session.query(Measurement.tobs).order_by(Measurement.station).\
+           filter(Measurement.date >= one_year_ago).all()
+
     return jsonify(list(np.ravel(tobs)))
 
 @app.route("/api/v1.0/<start>")
 def calc_start_temp(start):
     calc = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-        filter(Measurement.date >= start).all()
+           filter(Measurement.date >= start).all()
+
     return jsonify(calc[0])
 
 @app.route("/api/v1.0/<start>/<end>")
 def calc_start_end_temp(start, end):
     calc = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+           filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
     return jsonify(calc[0])
 
 
