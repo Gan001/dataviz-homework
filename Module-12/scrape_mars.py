d4ed26f816f7add22109850c770a87968929c0a6
Ganesh Crooks <gcroo001@gmail.com>
2019-04-20 19:55:08 -0400

    begin loading to mongo

--- Module-12/scrape_mars.py
+++ Module-12/scrape_mars.py
@@ -1,7 +1,7 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import pandas as pd
-import pymongo
+#import pymongo
 
 def init_browser():
     executable_path = {'executable_path': 'chromedriver.exe'}
