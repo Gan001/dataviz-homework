423f57d4edbd0b249dfc22a8dad165ce5529725b
Ganesh Crooks <gcroo001@gmail.com>
2019-04-27 18:15:57 -0400

    complete multi filter

--- Module-14/StarterCode/static/js/app.js
+++ Module-14/StarterCode/static/js/app.js
@@ -33,19 +33,100 @@ function buildTable(tables){
       }
 
 }
+// function isEmpty(input){
+//     var value = input.property("value");
+//     return value;
+// }
+// function checkFilter(){
+//     var inputDate = d3.select("#datetime");
+//     var inputCity = d3.select("#city");
+//     var dateBool = true;
+//     var cityBool = true;
+    
+//   // Get the value property of the input element
+//   var dateValue = inputDate.property("value");
 
+//   var cityValue = inputCity.property("value");
+//   if (cityValue === ""&& dateValue === ""){
+//     dateBool = false;
+//     cityBool = false;
+// }
+// return cityBool && dateBool;
+
+// }
 var submit = d3.select("#filter-btn");
 
 submit.on("click",function(){
     // Prevent the page from refreshing
     d3.event.preventDefault();
     // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
-
+  //var inputElement = d3.select("#datetime");
+  var inputDate = d3.select("#datetime");
+  var inputCity = d3.select("#city");
+  var inputState = d3.select("#state");
+  var inputCountry = d3.select("#country");
+  var inputShape = d3.select("#shape");
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  //var inputValue = inputElement.property("value");
+//var filteredData;
+  var dateValue = inputDate.property("value");
+    var cityValue = inputCity.property("value");
+    var stateValue = inputState.property("value");
+    var countryValue = inputCountry.property("value");
+    var shapeValue = inputShape.property("value");
+  //var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+//   if (dateValue && cityValue ){
+//      filteredData = tableData.filter(tableData => tableData.datetime === dateValue && tableData.city === cityValue);
+//   }
+//   else if (dateValue){
+//     filteredData = tableData.filter(tableData => tableData.datetime === dateValue);
+//   }
+//   else if (cityValue){
+//     filteredData = tableData.filter(tableData => tableData.city === cityValue);
+//   }
+    
+  var filteredData = tableData.filter(tableData => {
+    var cityBool = false;
+    var dateBool = false;
+    var stateBool = false;
+    var countryBool = false;
+    var shapeBool = false;
+    if( tableData.city === cityValue){
+         cityBool = true;
+    } else if(!cityValue){
+        cityBool = true;
+    }
 
-  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    if( tableData.datetime === dateValue){
+        dateBool = true;
+   } else if (!dateValue){
+        dateBool = true;
+   }
+
+   if( tableData.state === stateValue){
+    stateBool = true;
+} else if(!stateValue){
+   stateBool = true;
+}
+
+if( tableData.country === countryValue){
+    countryBool = true;
+} else if(!countryValue){
+   countryBool = true;
+}
+
+if( tableData.shape === shapeValue){
+    shapeBool = true;
+} else if(!shapeValue){
+   shapeBool = true;
+}
+
+    return(cityBool && dateBool && stateBool && countryBool && shapeBool);
+  });
+//   inputElement = d3.select("#city");
+//   inputValue = inputElement.property("value");
+//   filteredData = filteredData.filter(filteredData => filteredData.city === inputValue);
+    //return(dateBool && cityBool && )
   console.log(filteredData);
   //clear previous table
   d3.select('#ufo-table tbody').html(' ')
